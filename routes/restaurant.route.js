5b9383eadf3e8a46f4f37687899a0feeaca6190f
chairil ashar <asharchairil4@gmail.com>
2023-05-24 16:13:52 +0700

    rstrnt service

--- routes/restaurant.route.js
+++ routes/restaurant.route.js
@@ -1,9 +1,18 @@
 var express = require("express");
-const { getAllRestaurant } = require("../services/restaurant.services");
+const {
+  getAllRestaurant,
+  getOneRestaurantById,
+} = require("../services/restaurant.services");
 var router = express.Router();
 
 router.get("/", async (req, res) => {
   let response = await getAllRestaurant();
+  res.json(response);
+});
+
+router.get("/:restaurantId", async (req, res) => {
+  let restaurantId = req?.params?.restaurantId;
+  let response = await getOneRestaurantById(restaurantId);
   res.json(response);
 });
 
